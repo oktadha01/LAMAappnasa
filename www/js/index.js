c00716037e072cf78c1b749241c18f778572d0d3
oktadha01 <48429219+oktadha01@users.noreply.github.com>
2019-04-15 14:07:22 +0700

    Revisi1

--- www/js/index.js
+++ www/js/index.js
@@ -3,7 +3,7 @@ var app = {
     // Application Constructor
     initialize: function() {
     
-        var url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
+        var url = "https://api.nasa.gov/planetary/apod?api_key=Qokswuuo1xizqbnJ7MgcjAIhXTTQwcfkIRUSQ6pw";
         
         $("#loadImage").click(function(){
             $.ajax({
